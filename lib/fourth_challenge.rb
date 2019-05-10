0f1a9eb8547b6e59051d45d33e84423d767f84e7
Maasa Kono <konomaasa@gmail.com>
2019-05-10 21:29:05 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,6 +7,7 @@ def third_challenge
       :hero_friends => [
         {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
